8486b80e07ac73f30daa41f222b5b5a1a772c2dd
Abhay <abhaydubey2767@gmail.com>
2024-02-23 09:21:02 +0530

    starting

---
+++ frontend/src/Component/Navbar.jsx
@@ -0,0 +1,15 @@
+import logo from '../Images/logopng.png'
+
+
+const Navbar = () => {
+
+  return (
+    <>
+    <div className='Header'>
+        <img src={logo} alt="logo" style={{ height:30, marginLeft: 25 }} />
+    </div>
+    </>
+  )
+}
+
+export default Navbar
